87571b6157ddd8dacae9fcf443b95e4fdc7b1944
Busko Ilya <trassae95st@mail.ru>
2022-12-10 15:52:45 +0300

    Create menu

--- src/components/blocks/Layout.js
+++ src/components/blocks/Layout.js
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+import Nav from './Nav';
 
 function Layout() {
   return (
     <>
       <Header/>
+      <Nav/>
       <Main>
         <Outlet/>
       </Main>
---
+++ src/components/blocks/Nav.js
@@ -0,0 +1,59 @@
+import { NavLink } from 'react-router-dom';
+
+import './Nav.css';
+
+const setClass = ({ isActive }) => isActive ? "nav__item nav__item_active" : "nav__item";
+
+function Nav() {
+  return (
+      <nav className = "nav">
+        <ul className = "nav__ul">
+          <li><NavLink className = { setClass } to = "/">Главная</NavLink></li>
+          <li><NavLink className = { setClass } to = "/photos">Фото</NavLink></li>
+          <li className = "nav__li">
+            <NavLink className = { setClass } to = "/info">Информация</NavLink>
+            <ul className = "nav__sub-ul">
+              <li><NavLink className = "nav__sub-item" to = "/info/contacts">Контакты</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/info/ranks">Разряды</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/info/info-board">Доска информации</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/info/news">Новости</NavLink></li>
+            </ul>
+          </li>
+          <li><NavLink className = { setClass } to = "/players">Игроки</NavLink></li>
+          <li className = "nav__li">
+            <NavLink className = { setClass }  to = "/tournaments">Турниры</NavLink>
+            <ul className = "nav__sub-ul">
+              <li><NavLink className = "nav__sub-item" to = "/tournaments/by-tournaments">Наши турниры</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/tournaments/world-championships">Чемпионаты мира</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/tournaments/euro-championships">Чемпионаты европы</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/tournaments/eu-2019-2020">Командный ЧЕ 2019-2020</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/tournaments/eu-2019-2020">Командный ЧЕ 2020-2021</NavLink></li>
+            </ul>
+          </li>
+          <li className = "nav__li">
+            <NavLink className = { setClass } to = "/study">Обучение</NavLink>
+            <ul className = "nav__sub-ul">
+              <li><NavLink className = "nav__sub-item" to = "/study/go-schools">Школы Го</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/study/go-books">Книги по Го</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/study/go-courses">Обучающие курсы</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/study/go-stories">Рассказы о Го</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/study/go-wiki">Справочник Го</NavLink></li>
+            </ul>
+          </li>
+          <li className = "nav__li">
+            <NavLink className = { setClass } to = "/belarus">Беларусь</NavLink>
+            <ul className = "nav__sub-ul">
+              <li><NavLink className = "nav__sub-item" to = "/belarus/minsk">Минск</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/belarus/brest">Брест</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/belarus/vitebsk">Витебск</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/belarus/gomel">Гомель</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/belarus/grodno">Гродно</NavLink></li>
+              <li><NavLink className = "nav__sub-item" to = "/belarus/mogilev">Могилёв</NavLink></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+  );
+}
+
+export default Nav;
