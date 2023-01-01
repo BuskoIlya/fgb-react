a606e44d2698e7122feb87de25f74b858b75373e
Busko Ilya <trassae95st@mail.ru>
2023-01-01 20:20:27 +0300

    Get players page data from db

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -1,45 +1,43 @@
 import React from 'react';
-
 import TitleLayout from '../components/blocks/TitleLayout';
-
-import '../css/table.css';
 import './Players.css';
+import '../css/table.css';
 
 function Players() {
 
-  const apiPlayers = process.env.REACT_APP_SERVER_URL
+  const url = process.env.REACT_APP_SERVER_URL
       + process.env.REACT_APP_API_PLAYERS;
   const [players, setPlayers] = React.useState([]);
   React.useEffect(() => {
-    fetch(apiPlayers)
+    fetch(url)
         .then(response => response.json())
         .then(data => setPlayers(data))
         .catch(e =>  console.log(e));
   }, []);
 
   return (
-      <TitleLayout title = "Игроки Беларуси">
-        <section className = "players">
-          <table className = "table table_blue">
+      <TitleLayout title="Игроки Беларуси">
+        <section className="players">
+          <table className="table table_blue">
             <thead>
             <tr>
-              <th className = "table__cell">№</th>
-              <th className = "table__cell">ФИО</th>
-              <th className = "table__cell">Город</th>
-              <th className = "table__cell">Разряд</th>
-              <th className = "table__cell">Рейтинг</th>
+              <th className="table__cell">№</th>
+              <th className="table__cell">ФИО</th>
+              <th className="table__cell">Город</th>
+              <th className="table__cell">Разряд</th>
+              <th className="table__cell">Рейтинг</th>
             </tr>
             </thead>
             <tbody>
             {
               players.map((item, index) =>
-                  <tr key = {index + 1}>
-                    <td className = "table__cell">{index + 1}</td>
-                    <td className = "table__cell table__cell_left">{item.name}</td>
-                    <td className = "table__cell">{item.city}</td>
-                    <td className = "table__cell">{item.rank}</td>
-                    <td className = "table__cell">{item.score}</td>
-                  </tr>
+                <tr key = {index + 1}>
+                  <td className="table__cell">{index + 1}</td>
+                  <td className="table__cell table__cell_left">{item.fio}</td>
+                  <td className="table__cell">{item.city}</td>
+                  <td className="table__cell">{item.sport_rank}</td>
+                  <td className="table__cell">{item.score}</td>
+                </tr>
               )
             }
             </tbody>
