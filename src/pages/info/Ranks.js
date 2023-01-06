f5aac75ce0ffb07c6b70e7bd0326f45b84431ed0
Busko Ilya <trassae95st@mail.ru>
2023-01-06 18:11:34 +0300

    Refactoring. Use data for ranks page from db

--- src/pages/info/Ranks.js
+++ src/pages/info/Ranks.js
@@ -5,33 +5,32 @@ import './Ranks.css';
 import TitleLayout from '../../components/blocks/TitleLayout';
 
 function Ranks() {
-
-  const apiRanksUrl = process.env.REACT_APP_SERVER_URL
+  const url = process.env.REACT_APP_SERVER_URL
       + process.env.REACT_APP_API_RANKS;
-  const [ranks, setRanks] = React.useState([]);
+  const [data, setData] = React.useState([]);
   React.useEffect(() => {
-    fetch(apiRanksUrl)
+    fetch(url)
         .then(response => response.json())
-        .then(data => setRanks(data))
+        .then(data => setData(data))
         .catch(e =>  console.log(e));
   }, []);
 
   return (
-      <TitleLayout title = "Система разрядов в Го">
-        <section className = "ranks">
-          <table className = "table table_blue table_size-50">
+      <TitleLayout title="Система разрядов в Го">
+        <section className="ranks">
+          <table className="table table_blue table_size-50">
             <thead>
             <tr>
-              <th className = "table__cell">Очки</th>
-              <th className = "table__cell">Разряды кю / дан</th>
+              <th className="table__cell">Очки</th>
+              <th className="table__cell">Разряды кю / дан</th>
             </tr>
             </thead>
             <tbody>
             {
-              ranks.map((item, index) =>
-                  <tr key = {index + 1}>
-                    <td className = "table__cell">{item.scores}</td>
-                    <td className = "table__cell">{item.rank}</td>
+              data.map((item, index) =>
+                  <tr key={index + 1}>
+                    <td className="table__cell">{item.scores}</td>
+                    <td className="table__cell">{item.sport_rank}</td>
                   </tr>
               )
             }
